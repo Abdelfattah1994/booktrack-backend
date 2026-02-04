3b8c56f71d199aefc13bdaf7931926983e775695
Abdel Fattah <abdelfattahrabou@gmail.com>
2026-02-04 11:51:28 +0100

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/booktrack/backend/BooktrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.booktrack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktrackBackendApplication.class, args);
+	}
+
+}
